624e9d7001ffebe672551056590976e176f55e84
arbolianmentirososian <mati8400@gmail.com>
2021-03-25 11:51:21 +0100

    unit test added

--- src/math_app/math_app.py
+++ src/math_app/math_app.py
@@ -13,14 +13,25 @@ def add(num1, num2):
     response = {
         "num1": num1,
         "num2": num2,
-        "num1 - num2": num1 + num2,
+        "num1 + num2": num1 + num2,
     }
 
     return make_response(jsonify(response), 200)
 
 
 @app.route("/subtract/<int:num1>/<int:num2>", methods=['GET'])
-def add(num1, num2):
+def subtract(num1, num2):
+    response = {
+        "num1": num1,
+        "num2": num2,
+        "num1 - num2": num1 - num2,
+    }
+
+    return make_response(jsonify(response), 200)
+
+
+@app.route("/multiply/<int:num1>/<int:num2>", methods=['GET'])
+def multiply(num1, num2):
     response = {
         "num1": num1,
         "num2": num2,
@@ -30,19 +41,8 @@ def add(num1, num2):
     return make_response(jsonify(response), 200)
 
 
-@app.route("/multiply/<int:num1>/<int:num2>", methods=['GET'])
-def add(num1, num2):
-    response = {
-        "num1": num1,
-        "num2": num2,
-        "num1 + num2": num1 - num2,
-    }
-
-    return make_response(jsonify(response), 200)
-
-
 @app.route("/divide/<int:num1>/<int:num2>", methods=['GET'])
-def add(num1, num2):
+def divide(num1, num2):
     response = {
         "num1": num1,
         "num2": num2,
---
+++ test/test_math_app.py
@@ -0,0 +1,40 @@
+import sys
+
+sys.path.insert(0, 'C:\\projects\\python\\python_pipeline_deploy\\src')
+
+from math_app import app
+
+test_client = app.test_client()
+
+def test_hello():
+    response = test_client.get("/")
+    assert response.status_code == 200
+    assert 'Hello, world!' in response.get_data().decode("utf-8")
+
+
+def test_add():
+    response = test_client.get("/add/10/20")
+    assert response.status_code == 200
+    result = response.get_json()["num1 + num2"]
+    assert result == 30
+
+
+def test_subtract():
+    response = test_client.get("/subtract/35/20")
+    assert response.status_code == 200
+    result = response.get_json()["num1 - num2"]
+    assert result == 15
+
+
+def test_multiply():
+    response = test_client.get("/multiply/10/20")
+    assert response.status_code == 200
+    result = response.get_json()["num1 * num2"]
+    assert result == 200
+
+
+def test_divide():
+    response = test_client.get("/divide/100/20")
+    assert response.status_code == 200
+    result = response.get_json()["num1 / num2"]
+    assert result == 5
