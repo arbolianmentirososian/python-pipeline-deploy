56e382968ad7dcce9318d59949ccc6026b07df2c
arbolianmentirososian <mati8400@gmail.com>
2021-03-29 14:19:41 +0200

    formatting corrected

--- src/math_app/__init__.py
+++ src/math_app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
- from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
- csrf = CSRFProtect(app)
+csrf = CSRFProtect(app)
  
 
 from math_app import math_app
